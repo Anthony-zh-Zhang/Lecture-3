3c6e61829123fb5ae037e7f9dd3ba6b99e380ae0
Max Harper <maxharp3r@gmail.com>
2017-09-13 10:27:53 -0500

    examples ready for class

--- scripts/local.js
+++ scripts/local.js
@@ -1,3 +1,17 @@
+/*
+document ready
+selectors
+console.log
+.hide
+parseInt() and .css('marginLeft')
+(fun css: transition: all 0.2s;)
+*/
+
+$(document).ready(function() {
+
+});
+
+// shorthand
 $(function() {
 
 });
