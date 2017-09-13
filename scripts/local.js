0f4430bac132ffbe3cb4341d31feedcf5199cba3
Max Harper <maxharp3r@gmail.com>
2017-09-13 12:32:50 -0500

    final state of lecture

--- scripts/local.js
+++ scripts/local.js
@@ -7,11 +7,19 @@ parseInt() and .css('marginLeft')
 (fun css: transition: all 0.2s;)
 */
 
-$(document).ready(function() {
-
-});
-
-// shorthand
 $(function() {
+
+  var clickHandler = function() {
+    console.log('hi');
+
+    var left = $('#a').css('marginLeft');
+    var leftPx = parseInt(left) + 100;
+    $('.boxy').css('marginLeft', leftPx + 'px');
+
+  }
+
+  $('#move-button').click(clickHandler);
+
+
 
 });
